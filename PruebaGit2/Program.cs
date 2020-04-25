d41e6a1372e7918041714035db9644a92855fdda
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2020-04-25 14:54:32 -0700

    Desde master

--- PruebaGit2/Program.cs
+++ PruebaGit2/Program.cs
@@ -7,6 +7,7 @@ namespace PruebaGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Hola GitHub desde local");
+            Console.WriteLine("Desde master");
         }
     }
 }
