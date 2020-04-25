26ad0b4dc604b720b5da680ec14d4f4e990ac24f
Antonio Rogel <arogel@unikino.edu.mx>
2020-04-25 14:34:50 -0700

    Agregar archivos de proyecto.

---
+++ PruebaGit2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaGit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
